bb9b48055796c1a15a6788cc469a0fa9a2c19db1
hhcc05 <62152097+hhcc05@users.noreply.github.com>
2022-12-08 20:52:45 +0900

    Create LCD.h

---
+++ LCD.h
@@ -0,0 +1,7 @@
+void LCD_Comm(char command);
+void LCD_Data(char ch);
+void LCD_PORT_Init(void);
+void LCD_Init(void);
+void LCD_Pos(unsigned char x, unsigned char y);
+void LCD_Char(unsigned char ch);
+void LCD_STR(unsigned char *str);
